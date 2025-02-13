6fe77f9b26bebc59806fd013763c2653865bf60b
Mohsen <mohsen.hosseini84@gmail.com>
2025-02-13 18:03:07 +0330

    scss to css

--- src/app/module/calendar/calendar.module.ts
+++ src/app/module/calendar/calendar.module.ts
@@ -4,14 +4,21 @@ import { CommonModule } from '@angular/common';
 // import { CalendarRoutingModule } from './calendar-routing.module';
 import { CalendarComponent } from './calendar/calendar.component';
 import { CalendarRoutingModule } from './calendar-routing.module';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTimepickerModule} from '@angular/material/timepicker';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     CalendarComponent,
-    CalendarRoutingModule
+    CalendarRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTimepickerModule,
+    MatDatepickerModule,
   ]
 })
 export class CalendarModule { }
--- src/app/module/calendar/calendar/calendar.component.ts
+++ src/app/module/calendar/calendar/calendar.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatTimepickerModule} from '@angular/material/timepicker';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {FormsModule} from '@angular/forms';
+
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatTimepickerModule,
+    MatDatepickerModule,
+    FormsModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+  styleUrl: './calendar.component.css'
 })
 export class CalendarComponent {
+
+  value!: Date;
 
 }
