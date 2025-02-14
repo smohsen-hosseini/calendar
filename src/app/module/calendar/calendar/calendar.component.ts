429d0ab5689c0a6c895e5fed5c728e00ada18c2b
Mohsen <mohsen.hosseini84@gmail.com>
2025-02-14 11:49:54 +0330

    datePicker and time ok

--- src/app/module/calendar/calendar/calendar.component.ts
+++ src/app/module/calendar/calendar/calendar.component.ts
@@ -1,8 +1,8 @@
-import {ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component,OnInit  } from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {FormsModule, ReactiveFormsModule,FormControl  } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule,FormControl  } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 // import { MaterialTimePickerComponent } from '@candidosales/material-time-picker'; //third-party-library
 
@@ -22,12 +22,40 @@ import { MatNativeDateModule } from '@angular/material/core';
   templateUrl: './calendar.component.html',
   styleUrl: './calendar.component.css'
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
+
+  calendarForm: FormGroup;
 
 
 
-  dateControl = new FormControl(new Date());
-  date: Date = new Date();
+  constructor(private fb: FormBuilder) {
+    this.calendarForm = this.fb.group({
+      dateControl: [new Date(), Validators.required],
+      timeControl: ['', Validators.required],
+    });
+  }
 
+  // dateControl = new FormControl(new Date());
+  // date: Date = new Date();
 
+  // timeControl = new FormControl('');
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (this.calendarForm.valid) {
+      console.log(this.calendarForm.value);
+    }
+  }
+
+  
+  get dateControl(): FormControl {
+    return this.calendarForm.get('dateControl') as FormControl;
+  }
+
+  get timeControl(): FormControl {
+    return this.calendarForm.get('timeControl') as FormControl;
+  }
+  
 }
